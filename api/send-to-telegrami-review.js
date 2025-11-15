ac0432c971bbd0f2c4fac14fa3e679351ce1afbb
Sasha_Omelucik <75577488+BakaSoCute@users.noreply.github.com>
2025-11-15 03:42:25 +0300

    Update send-to-telegrami-review.js

--- api/send-to-telegrami-review.js
+++ api/send-to-telegrami-review.js
@@ -7,10 +7,16 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+const allowedOrigins = [
+  'https://www.nyamuras-santa.ru',
+  'http://localhost:5173'
+];
+const origin = req.headers.origin;
+if (allowedOrigins.includes(origin)) {
+  res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+  }
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
