7107e7a70ad098b8a4b2d3f5fb6c93efa4135289
Sasha_Omelucik <75577488+BakaSoCute@users.noreply.github.com>
2025-11-16 21:37:56 +0300

    Update twitch-auth.js

--- api/twitch-auth.js
+++ api/twitch-auth.js
@@ -70,7 +70,6 @@ export default async function handler(req, res) {
       res.setHeader('Set-Cookie', 
         `twitch_access_token=${tokenResponse.data.access_token}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=604800`
       );
-              console.log(res.json());
 
       return res.json({
         user: {
@@ -97,6 +96,7 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
 
 
