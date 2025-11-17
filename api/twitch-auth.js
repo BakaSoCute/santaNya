97437f9f0b2af5eafc1f2732c4fa15fb9b30b22b
Sasha_Omelucik <75577488+BakaSoCute@users.noreply.github.com>
2025-11-17 14:04:42 +0300

    Update twitch-auth.js

--- api/twitch-auth.js
+++ api/twitch-auth.js
@@ -73,7 +73,7 @@ export default async function handler(req, res) {
       if (isSafari) {
         console.log("its safari");
         // Для Safari используем SameSite=Lax
-        cookieOptions = `twitch_access_token=${tokenResponse.data.access_token}; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=604800; Domain=santa-nya.vercel.app`;
+        cookieOptions = `twitch_access_token=${tokenResponse.data.access_token}; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=604800; Domain=process.env.FRONT_URL`;
       } else {
         // Для других браузеров SameSite=None
         cookieOptions = `twitch_access_token=${tokenResponse.data.access_token}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=604800`;
@@ -114,6 +114,7 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
 
 
