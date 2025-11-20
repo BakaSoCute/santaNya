0d53447ff33477f8b5f5f33bdb2bf5a4dd58c1e6
Sasha_Omelucik <75577488+BakaSoCute@users.noreply.github.com>
2025-11-20 12:12:18 +0300

    Update send-to-telegram-img.js

--- api/send-to-telegram-img.js
+++ api/send-to-telegram-img.js
@@ -18,7 +18,7 @@ const origin = req.headers.origin;
 if (allowedOrigins.includes(origin)) {
   res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
 
